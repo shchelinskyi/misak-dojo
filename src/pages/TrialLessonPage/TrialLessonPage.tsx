070622b94279a58437887dc4c20594b75f5407e3
Artem Shchelinskyi <shchelinskyi@gmail.com>
2023-11-21 11:53:24 +0200

    add changes

--- src/pages/TrialLessonPage/TrialLessonPage.tsx
+++ src/pages/TrialLessonPage/TrialLessonPage.tsx
@@ -9,7 +9,6 @@ import {Image} from "react-bootstrap";
 
 const TrialLessonPage = () => {
     return (
-        <div className={s.wrapper}>
             <div className={s.container}>
                 <div className={s.block}>
                     <Image className={s.girlImg} src={girl}/>
@@ -17,15 +16,17 @@ const TrialLessonPage = () => {
                         <h5 className={s.centerItemTitle}>БЕЗКОШТОВНЕ <br/> ПРОБНЕ ЗАНЯТТЯ</h5>
                         <p className={s.centerItemDescription}>Якщо сумніваєшся, приходь на пробне тренування <br/>
                             та випробуй свої сили разом з нами!</p>
-                        <CustomButton onClick={() => console.log("check")} styles={{maxWidth:"292px", alignSelf:"center"}}> записатися на заняття</CustomButton>
+                        <div className={s.btnBlock}>
+                            <CustomButton onClick={() => console.log("check")}> записатися на заняття</CustomButton>
+                        </div>
+                        <Image className={s.cloudLeftImg} src={cloudLeft}/>
+                        <Image className={s.cloudRightTopImg} src={cloudRT}/>
+                        <Image className={s.cloudRightBottomImg} src={cloudRB}/>
                     </div>
                     <Image className={s.boyImg} src={boy}/>
+
                 </div>
-                <Image className={s.cloudLeftImg} src={cloudLeft}/>
-                <Image className={s.cloudRightTopImg} src={cloudRT}/>
-                <Image className={s.cloudRightBottomImg} src={cloudRB}/>
             </div>
-        </div>
     );
 };
 
